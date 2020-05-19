9afde810fea26d6328fef02175c23600028387dd
Vandana Singh <42498298+Vandana-Singh@users.noreply.github.com>
2020-05-19 22:15:50 +0530

    Update home.py

--- backend/app/home.py
+++ backend/app/home.py
@@ -16,28 +16,29 @@ home_bp = Blueprint('home_bp', __name__)
 
 @home_bp.route('/Home', methods = ['POST' , 'GET'])
 def home():
-    # fetching data from frontend
-    data = request.get_json()
-    threshold = data['threshold']
-    pid = data['pid']
-    id = current_user.id
-    print(threshold)
-    print(pid)
-    print(id)
-    # fetching product details from table
-    prod = Products.query.filter_by(pid=pid).first()
-    if threshold == "":
-        return "enter a non-empty value!"
-    else:
-        # value should be in range of 30% to 100%
-        # lower = params['lower_price'] * int(prod.mrp[1:])
-        lower = 0.3 * int(prod.mrp[1:])
-        if int(threshold) >= lower and int(threshold) <= int(prod.mrp[1:]):
-            entry = Waitlist(id=id, pid=pid, threshold=threshold)
-            db.session.add(entry)
-            db.session.commit()
-            return "True"
+    if current_user.is_active() and request.method == 'POST':
+        # fetching data from frontend
+        data = request.get_json()
+        threshold = data['threshold']
+        pid = data['pid']
+        id = current_user.id
+        print(threshold)
+        print(pid)
+        print(id)
+        # fetching product details from table
+        prod = Products.query.filter_by(pid=pid).first()
+        if threshold == "":
+            return "enter a non-empty value!"
         else:
-            return "Enter a value in range!"
+            # value should be in range of 30% to 100%
+            # lower = params['lower_price'] * int(prod.mrp[1:])
+            lower = 0.3 * int(prod.mrp[1:])
+            if int(threshold) >= lower and int(threshold) <= int(prod.mrp[1:]):
+                entry = Waitlist(id=id, pid=pid, threshold=threshold)
+                db.session.add(entry)
+                db.session.commit()
+                return "True"
+            else:
+                return "Enter a value in range!"
     return render_template("index.html", token="Hello   world")
 
