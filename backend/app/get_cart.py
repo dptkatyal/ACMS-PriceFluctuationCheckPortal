dccc828f693f43aba24a4351ebdd44e73c03e6ef
Vandana Singh <42498298+Vandana-Singh@users.noreply.github.com>
2020-05-21 16:38:12 +0530

    Update get_cart.py

--- backend/app/get_cart.py
+++ backend/app/get_cart.py
@@ -2,5 +2,18 @@ from flask import Blueprint, jsonify, json
 from flask_login import current_user, login_required
 from werkzeug.security import check_password_hash
 from .models import db, Product
+from .models import db, Waitlist
+from .models import db, User
 from sqlalchemy import update
-prod_bp = Blueprint('prod_bp', __name__)
+cart_bp = Blueprint('cart_bp', __name__)
+
+@cart_bp.route('/customer' , methods = ['GET'])
+def get_cart():
+    id = current_user.id
+    prod = []
+    mylist = Waitlist.query.filter(Waitlist.id == id).all()
+    for pr in mylist:
+        data = Product.query.filter(Product.pid == pr.pid).first()
+        dict = {'pid':data.pid , 'name':data.name , 'mrp':data.mrp , 'price':data.price, 'img_file':data.img_file , 'slug': data.slug , 'description':data.description}
+        prod.append(dict)
+    return jsonify(prod)
