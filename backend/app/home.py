05130965b362fa8f8cc784492d762732e446f58c
Vandana Singh <42498298+Vandana-Singh@users.noreply.github.com>
2020-05-21 12:33:45 +0530

    home.py complete

--- backend/app/home.py
+++ backend/app/home.py
@@ -22,9 +22,9 @@ def home():
         threshold = data['threshold']
         pid = data['pid']
         id = current_user.id
-        print(threshold)
-        print(pid)
-        print(id)
+        #print(threshold)
+        #print(pid)
+        #print(id)
         # fetching product details from table
         prod = Products.query.filter_by(pid=pid).first()
         if threshold == "":
@@ -34,11 +34,20 @@ def home():
             # lower = params['lower_price'] * int(prod.mrp[1:])
             lower = 0.3 * int(prod.mrp[1:])
             if int(threshold) >= lower and int(threshold) <= int(prod.mrp[1:]):
-                entry = Waitlist(id=id, pid=pid, threshold=threshold)
-                db.session.add(entry)
-                db.session.commit()
-                return "True"
+                qe = Waitlist.query.filter((Waitlist.id == id) , (Waitlist.pid == pid)).first()
+                #if current request is not present in Waitlist
+                if qe is None:
+                    entry = Waitlist(id=id, pid=pid, threshold=threshold)
+                    db.session.add(entry)
+                    db.session.commit()
+                    return "True"
+                #if user have already set the threshold before
+                else:
+                    return "You already have this item in your waitlist.Check your waitlist to edit threshold."
             else:
                 return "Enter a value in range!"
+    #if user not logged in and request is POST
+    if current_user.is_active()==false and request.method == 'POST':
+        return "login first!"
     return render_template("index.html", token="Hello   world")
 
